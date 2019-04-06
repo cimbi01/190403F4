a3474fa4880da4a40bc3242adea67e7796adacca
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-04-06 15:31:39 +0200

    Add project files.

---
+++ 190403F4/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace _190403F4
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console. Press Ctrl+F5 (or go to
+            // Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        #endregion Private Methods
+    }
+}
